b44a6b5e9e03586789aa8cf56680f8e18d0c8da1
safafa <safaaballagh97@gmail.com>
2021-04-21 19:00:57 +0000

    Fix methods as required

--- main.rb
+++ main.rb
@@ -2,39 +2,64 @@
 module Enumerable
   def my_each
     i = 0
-    while i < size
-      yield(at(i))
+    unless block_given? then return enum_for end
+    arr = to_a 
+    while i < arr.size
+      yield(arr.at(i))
       i += 1
     end
+    self
   end
 
   def my_each_with_index
     i = 0
-    while i < size
-      yield(i, at(i))
+    unless block_given? then return enum_for end
+    arr = to_a if is_a? Range or is_a? Hash
+    while i < arr.size
+      yield(arr.at(i), i)
       i += 1
     end
+    self
   end
 
   def my_select
+    unless block_given? then return enum_for end
     selected = []
     my_each { |item| selected << item if yield(item) }
     selected
   end
 
-  def my_all?
+  def my_all?(args = nil)
     if block_given?
-      my_each { |item| return false unless yield(item) }
+      my_each { |item| return false if yield(item) == false }
+      return true
+    elsif args.nil?
+      my_each { |item| return false if item.nil? || item == false }
+    elsif !args.nil? && (args.is_a? Class)
+      my_each { |item| return false if item.class != args }
+    elsif !args.nil? && self.class == Hash
+      return false unless empty? 
+    elsif !args.nil? && args.class == Regexp
+      my_each { |item| return false unless args.match(item.to_s) }
     else
-      my_each { |item| return false unless item }
+      my_each { |item| return false if item != args }
     end
     true
   end
 
-  def my_any?
-    unless empty?
+  def my_any?(args = nil)
+    arr = to_a 
+    unless arr.empty?
       if block_given?
         my_each { |item| return true if yield(item) }
+      elsif !args.nil? && (args.is_a? Class)
+        my_each { |item| return true if item.class == args }
+      elsif !args.nil? && self.class == Hash
+        return false unless empty? 
+      elsif !args.nil? && args.class == Regexp
+        my_each { |item| return true if args.match(item.to_s) }
+      elsif args.nil?
+        my_each { |item| return true if item == args }
       else
         my_each { |item| return true if item }
       end
@@ -42,10 +67,17 @@ module Enumerable
     false
   end
 
-  def my_none?
-    unless empty?
+  def my_none?(args = nil)
+    arr = to_a 
+    unless arr.empty?
       if block_given?
         my_each { |item| return false if yield(item) }
+      elsif !args.nil? && (args.is_a? Class)
+        my_each { |item| return false if item.class == args } 
+      elsif !args.nil? && args.class == Regexp
+        my_each { |item| return false if args.match(item.to_s) }
+      elsif !args.nil?
+        my_each { |item| return false if item == args }
       else
         my_each { |item| return false if item }
       end
@@ -65,11 +97,21 @@ module Enumerable
     size
   end
 
-  def my_inject(args = 0)
-    result = at(args)
-    i = args + 1
-    while i < size
-      result = yield(result, at(i))
+  def my_inject(n = 0, s = nil)
+    arr = to_a
+    i = 0
+    result = n
+    unless block_given?
+      if n.class == Symbol or n.class == String 
+        result = 0
+        my_each { |item| result = item.send(n, result) }
+      elsif !s.nil? && s.class == Symbol
+        my_each { |item| result = item.send(s, result) }
+      end
+      return result
+    end
+    while i < arr.size
+      result = yield(result, arr.at(i))
       i += 1
     end
     result
@@ -77,13 +119,15 @@ module Enumerable
 
   def my_map(args = nil)
     map_array = []
-    if block_given?
-      my_each { |item| map_array << yield(item) }
-      return map_array
-    elsif !args.nil?
+    if !args.nil? 
       my_proc = args
       my_each { |item| map_array << my_proc.call(item) }
       return map_array
+    elsif block_given?
+      my_each { |item| map_array << yield(item) }
+      return map_array
+    else
+      return enum_for
     end
     self
   end
@@ -92,3 +136,6 @@ end
 def multiply_els(array)
   array.my_inject { |multi, n| multi * n }
 end
+
+puts (1..5).inject(2,:+)
+puts (1..5).my_inject(2,:+)
